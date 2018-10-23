af46da20479a67ad5d65fee617f8bf30ad4d7843
Francois <etu35530@student.henallux.be>
2018-10-23 16:59:02 +0200

    Création de la page de préférences + liaison bouton pour commencer l'activitée

--- app/src/main/java/com/henallux/smartcity/view/MainActivity.java
+++ app/src/main/java/com/henallux/smartcity/view/MainActivity.java
@@ -1,15 +1,43 @@
 package com.henallux.smartcity.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.henallux.smartcity.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button inscriptionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inscriptionButton = findViewById(R.id.InscriptionButton);
+
+        inscriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Preferences.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 1){
+            switch (resultCode){
+                case 1 : Toast.makeText(MainActivity.this, "Result code = 1", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
     }
 }
