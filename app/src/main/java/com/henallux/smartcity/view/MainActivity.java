e45c650f793d8caa4bcab58a291bf5c858355dc3
Francois <etu35530@student.henallux.be>
2018-10-23 16:28:06 +0200

    Ajout MainActivity

---
+++ app/src/main/java/com/henallux/smartcity/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.henallux.smartcity.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.henallux.smartcity.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
