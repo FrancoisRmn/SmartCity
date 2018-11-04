e3360f11b4cf1800bb4c06062047018ecc17a412
pozzi nicolas <etu31409@student.henallux.be>
2018-11-04 20:28:29 +0100

    ajout d'une bottom navigation view

---
+++ app/src/main/java/com/henallux/smartcity/view/BottomMenu.java
@@ -0,0 +1,51 @@
+package com.henallux.smartcity.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.henallux.smartcity.R;
+
+public class BottomMenu extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_menu);
+
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RestaurantFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_eat:
+                            selectedFragment = new RestaurantFragment();
+                            break;
+                        case R.id.nav_drink:
+                            selectedFragment = new DrinkFragment();
+                            break;
+                        case R.id.nav_market:
+                            selectedFragment = new MarketFragment();
+                            break;
+                        case R.id.nav_direction:
+                            selectedFragment = new DirectionFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
