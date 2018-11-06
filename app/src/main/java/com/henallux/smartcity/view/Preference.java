90713cbfde524487a5ebdd1efc10df7bb37b49dc
pozzi nicolas <etu31409@student.henallux.be>
2018-11-06 16:37:09 +0100

    Ajout d'un intent à préférence vers bottomMenu (menu principale)

--- app/src/main/java/com/henallux/smartcity/view/Preference.java
+++ app/src/main/java/com/henallux/smartcity/view/Preference.java
@@ -1,18 +1,30 @@
 package com.henallux.smartcity.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.henallux.smartcity.R;
 
 public class Preference extends AppCompatActivity {
+    private Button connectionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        connectionButton = findViewById(R.id.creationButton);
+        connectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Preference.this,BottomMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onCheckboxClicked(View view) {
