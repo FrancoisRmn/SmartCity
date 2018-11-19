388d527fe098af07c17ee1918fca978483a0c378
pozzi nicolas <etu31409@student.henallux.be>
2018-11-19 17:20:06 +0100

    supp de mon test ...

--- app/src/main/java/com/henallux/smartcity/view/DirectionFragment.java
+++ app/src/main/java/com/henallux/smartcity/view/DirectionFragment.java
@@ -7,13 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.henallux.smartcity.R;
 
 public class DirectionFragment extends Fragment {
+    private Button mButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_direction, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mButton = (Button)getView().findViewById(R.id.buttonTestFragment);
+        mButton.setText("TEST123");
     }
 }
