cb9d25f4046fec8bbc8aadbfe03bbcfc7818637a
pozzi nicolas <etu31409@student.henallux.be>
2018-12-26 18:42:40 +0100

    Possibilité de naviguer entre plrs images (rien que pour les restaurants pour l'instant)

--- app/src/main/java/com/henallux/smartcity/view/ElementDetailFragmentRestaurant.java
+++ app/src/main/java/com/henallux/smartcity/view/ElementDetailFragmentRestaurant.java
@@ -199,7 +199,6 @@ public class ElementDetailFragmentRestaurant extends Fragment {
 
         }
         else{
-            //TODO cacher le bouton
             buttonNextImage.setVisibility(View.GONE);
         }
 
