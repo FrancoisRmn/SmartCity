ab5ddd6fab3ce0f1fccda0860f6397b494bc36f7
Francois <etu35530@student.henallux.be>
2019-01-06 15:02:04 +0100

    Supression des imports inutils dans le package service + correction orthographe logger ServiceBuilder

--- app/src/main/java/com/henallux/smartcity/service/FirebaseMessagingService.java
+++ app/src/main/java/com/henallux/smartcity/service/FirebaseMessagingService.java
@@ -1,21 +1,16 @@
 package com.henallux.smartcity.service;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-
 import com.google.firebase.messaging.RemoteMessage;
 import com.henallux.smartcity.R;
 import com.henallux.smartcity.view.MainActivity;
 
-import static android.support.constraint.Constraints.TAG;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
     public FirebaseMessagingService() {
@@ -33,17 +28,15 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     }
 
     @Override
-    public void onDeletedMessages() {
-
-    }
+    public void onDeletedMessages() { }
 
     private void sendNotification(String title,String messageBody) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
@@ -55,6 +48,6 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
